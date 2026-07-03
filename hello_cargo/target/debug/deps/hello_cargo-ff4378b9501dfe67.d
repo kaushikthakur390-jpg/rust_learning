99f75480cd0063a0647a9b34e0c1cc888f620f8c
kaushikthakur390-jpg <kaushikthakur390@gmail.com>
2026-07-04 02:58:25 +0530

    learnt about cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-ff4378b9501dfe67.d
@@ -0,0 +1,5 @@
+/home/kosik/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-ff4378b9501dfe67.d: src/main.rs
+
+/home/kosik/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-ff4378b9501dfe67: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/kosik/Documents/rust/hello_cargo/target/debug/hello_cargo: /home/kosik/Documents/rust/hello_cargo/src/main.rs
