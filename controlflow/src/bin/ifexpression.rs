537dd42c85fb3e3ef7b649a31fe194c929aef7db
kaushikthakur390-jpg <kaushikthakur390@gmail.com>
2026-09-09 01:51:46 +0530

    learnt if expression in rust

---
+++ controlflow/src/bin/ifexpression.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let number = 3;
+    if number < 5 { //the condtion should always be a bool type
+        println!("the condition is true");
+    } else {
+        println!("the condition is false");
+    }
+}
